5ffd0c8f2954cba258f17e29cff162284f997349
Stuart Murray <stuart.murray.dev@outlook.com>
2024-06-24 19:05:25 +0100

    simplify controller. Add mediator for details of activity

--- Reactivities/API/Controllers/ActivitiesController.cs
+++ Reactivities/API/Controllers/ActivitiesController.cs
@@ -1,30 +1,21 @@
-﻿using Application.Activities;
+﻿using Application;
+using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers;
 
 public class ActivitiesController : BaseApiController
 {
-    private readonly IMediator _mediator;
-
-    public ActivitiesController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
-
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await _mediator.Send(new List.Query());
+        return await Mediator.Send(new List.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity?>> GetActivity(Guid id)
     {
-        return Ok();
+        return await Mediator.Send(new Details.Query { Id = id });
     }
 }
