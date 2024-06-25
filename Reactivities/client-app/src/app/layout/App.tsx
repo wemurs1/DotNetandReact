b0ac04eea15fd141f0449cde9b9c0bf73c75305b
Stuart Murray <stuart.murray.dev@outlook.com>
2024-06-25 16:37:14 +0100

    checkpoint

---
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { List } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+import NavBar from "./NavBar";
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <NavBar />
+      <List>
+        {activities.map((activity) => {
+          return <List.Item key={activity.id}>{activity.title}</List.Item>;
+        })}
+      </List>
+    </div>
+  );
+}
+
+export default App;
