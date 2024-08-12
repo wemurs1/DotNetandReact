c7c3c746d3cab08433f7625ba9c9d7746706485e
Stuart Murray <stuart.murray.dev@outlook.com>
2024-08-12 14:38:17 +0100

    remove stacktrace from production error page

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -29,10 +29,10 @@ public class ExceptionMiddleware
             context.Response.ContentType = "application/json";
             context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-            // var response = _env.IsDevelopment()
-            //     ? new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
-            //     : new AppException(context.Response.StatusCode, "Internal server error");
-            var response = new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString());
+            var response = _env.IsDevelopment()
+                ? new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                : new AppException(context.Response.StatusCode, "Internal server error");
+            // var response = new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString());
 
             var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
