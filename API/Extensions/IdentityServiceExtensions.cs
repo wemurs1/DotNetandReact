60dc9499432b52875386b3dbf28297709883064d
Stuart Murray <stuart.murray.dev@outlook.com>
2024-08-13 17:58:48 +0100

    checkpoint

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -28,7 +28,9 @@ public static class IdentityServiceExtensions
                 ValidateIssuerSigningKey = true,
                 IssuerSigningKey = key,
                 ValidateIssuer = false,
-                ValidateAudience = false
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
             };
             options.Events = new JwtBearerEvents
             {
