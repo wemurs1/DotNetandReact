fd0b3335c4deb880ef45c98e8d1f1b87b14c469b
Stuart Murray <stuart.murray.dev@outlook.com>
2024-08-15 11:39:47 +0100

    change to more real world token expiry and refresh interval

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -32,7 +32,7 @@ public class TokenService
         var tokenDescriptor = new SecurityTokenDescriptor
         {
             Subject = new ClaimsIdentity(claims),
-            Expires = DateTime.UtcNow.AddMinutes(1),
+            Expires = DateTime.UtcNow.AddMinutes(10),
             SigningCredentials = creds
         };
 
